8ddd4d68aa3018a2e12b584549de0b763ef9654c
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-07-03 14:32:54 +0430

    added changes to prod and stage

--- Dockerfiles/prod/django/entrypoint.sh
+++ Dockerfiles/prod/django/entrypoint.sh
@@ -4,19 +4,21 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
-exec "$@"
+
 
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+
+exec "$@"
 
 # DJANGO_SUPERUSER_USERNAME=admin \
 # DJANGO_SUPERUSER_EMAIL=admin@admin.com \
--- debian_installer.sh
+++ debian_installer.sh
@@ -5,8 +5,15 @@ sudo apt-get upgrade -y
 
 sudo apt-get -y install --reinstall make
 sudo apt-get -y install curl
+sudo apt-get install git
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo usermod -a -G docker $(whoami)
-sudo service docker start
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo systemctl restart network-manager
+sudo systemctl restart docker
