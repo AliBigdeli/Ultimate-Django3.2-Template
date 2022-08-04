115f1db5149fa52da57f55c85e03e95a492abe5f
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-04 21:53:12 +0400

    added corsheader middleware

--- core/core/settings.py
+++ core/core/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
