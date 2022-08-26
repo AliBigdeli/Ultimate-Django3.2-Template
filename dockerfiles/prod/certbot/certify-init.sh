88553a16450a1a0d0abe5c020bb9465fa7265e59
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-26 15:10:29 +0400

    testing new changes

--- dockerfiles/prod/certbot/certify-init.sh
+++ dockerfiles/prod/certbot/certify-init.sh
@@ -13,9 +13,11 @@ echo "Getting certificate..."
 
 certbot certonly \
     --webroot \
-    --webroot-path "/vol/www/" \
+    -w "/vol/www/" \
     -d "$DOMAIN" \
     --email $EMAIL \
+    --force-renewal \
     --rsa-key-size 4096 \
     --agree-tos \
     --noninteractive
+
--- dockerfiles/prod/nginx/entrypoint.sh
+++ dockerfiles/prod/nginx/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 echo "Checking for dhparams.pem"
 if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
   echo "dhparams.pem does not exist - creating it"
-  openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+  openssl dhparam -out /vol/proxy/ssl-dhparams.pem 1024
 fi
 
 echo "Checking for fullchain.pem"
