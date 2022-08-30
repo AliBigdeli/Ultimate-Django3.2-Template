677fdb7a287e4b824aea696593a81d0351988b64
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-30 16:23:47 +0400

    Update entrypoint.sh

--- dockerfiles/prod/nginx/entrypoint.sh
+++ dockerfiles/prod/nginx/entrypoint.sh
@@ -11,10 +11,10 @@ fi
 echo "Checking for fullchain.pem"
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
   echo "No SSL cert, enabling HTTP only..."
-  envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+  envsubst '\$DOMAIN' < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 else
   echo "SSL cert exists, enabling HTTPS..."
-  envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
+  envsubst '\$DOMAIN' < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
 fi
 
 nginx-debug -g 'daemon off;'
