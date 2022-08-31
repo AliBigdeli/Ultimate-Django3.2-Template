3d51e2f201c39f89bb84918a13b71476a36b89c5
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-31 16:40:31 +0400

    Update recreate_stage.sh

--- commands/recreate_stage.sh
+++ commands/recreate_stage.sh
@@ -1,5 +1,5 @@
 docker compose -f ./docker-compose-stage.yml down
 docker volume prune -f
-docker rmi $(docker images --format '{{.Repository}}' | grep 'ultimate')
+docker rmi $(docker images --format '{{.Repository}}' | grep '$PROJECT_NAME')
 git pull 
 docker compose -f docker-compose-stage.yml up --build -d
