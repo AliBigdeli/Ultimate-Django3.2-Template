e55931abbc54ce88fffd4342537febc65f18b06e
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-31 16:50:08 +0400

    adding run permission to shell commands

--- commands/recreate_prod.sh
+++ commands/recreate_prod.sh
@@ -1,6 +1,6 @@
 docker compose -f ./docker-compose-prod.yml down
 docker volume prune -f
-docker rmi $(docker images --format '{{.Repository}}' | grep 'ultimate')
+docker rmi $(docker images --format '{{.Repository}}' | grep '$PROJECT_NAME')
 git pull 
 docker compose -f docker-compose-prod.yml  run --rm certbot /opt/certify-init.sh
 docker compose -f docker-compose-prod.yml down
