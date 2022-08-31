05e5c6cbb288723d6739080ddb1bcd0338c0b47b
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-08-31 16:55:54 +0400

    Create create_envs.sh

---
+++ commands/create_envs.sh
@@ -0,0 +1,9 @@
+echo "creating env files for stage mode"
+cp ./envs/stage/db/.env_sample .env
+cp ./envs/stage/django/.env_sample .env
+
+
+echo "creating env files for production mode"
+cp ./envs/prod/db/.env_sample .env
+cp ./envs/prod/django/.env_sample .env
+cp ./envs/prod/nginx/.env_sample .env
